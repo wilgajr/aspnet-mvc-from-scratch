2863b5673cea27b8be19299aeca59d55699e6a91
wilgajr <wil.garcia@outlook.com>
2020-10-01 01:08:29 -0300

    Home Controller, some clean-up, node_modules exposed for development purposes and a txt with the commands that I used to bootstrap the whole thing

--- src/AspNetMvcFromScratch/Startup.cs
+++ src/AspNetMvcFromScratch/Startup.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
+using System.IO;
 
 namespace AspNetMvcFromScratch
 {
@@ -16,6 +14,7 @@ namespace AspNetMvcFromScratch
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +23,29 @@ namespace AspNetMvcFromScratch
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
 
+                // Expose node_modules folder for development cenarios
+                var nodeModulesPath = Path.Combine(Directory.GetCurrentDirectory(), "node_modules");
+
+                app.UseStaticFiles(new StaticFileOptions()
+                {
+                    FileProvider = new PhysicalFileProvider(nodeModulesPath),
+                    RequestPath = new PathString("/node_modules")
+                });
+            }
+            else
+            {
+                // TODO: 404 error page
+                app.UseStaticFiles();
+            }
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
